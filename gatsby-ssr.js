987c1b64e49a1cd6e552f36385d6e5306ee727a8
Clarice Bouwer <cbillowes@gmail.com>
2021-09-30 05:05:41 +0400

    remove comments indicating file can be deleted
    As the files are in use the comments are unnecessary and just added noise and confusion.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,7 +4,6 @@
  * See: https://www.gatsbyjs.com/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
 import RootWrapper from './src/components/ui/root-wrapper'
 
 export const wrapRootElement = RootWrapper
